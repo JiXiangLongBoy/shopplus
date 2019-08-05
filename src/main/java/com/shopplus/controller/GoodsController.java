05c80371e18e0951dd1f785bd54b01116910008f
JiXiangLongBoy <1020165019@qq.com>
2019-08-05 20:19:49 +0800

    完善index页面功能接口

--- src/main/java/com/shopplus/controller/GoodsController.java
+++ src/main/java/com/shopplus/controller/GoodsController.java
@@ -1,16 +1,25 @@
 package com.shopplus.controller;
 
 import com.shopplus.pojo.Goods;
+import com.shopplus.service.GoodsService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
 
-    @RequestMapping("/hotgoods")
-    public Goods hotGoods(){
+    @Autowired
+    GoodsService goodsService;
 
-        return null;
+    @ApiOperation("返回index页面热销的商品数据")
+    @RequestMapping("/hotgoods")
+    public List<Goods> hotGoods() {
+        List<Goods> hotsellGoods = goodsService.getHotsellGoods();
+        return hotsellGoods;
     }
 }
