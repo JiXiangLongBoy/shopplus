dc0457b6832bb8553c04a9eff887ee5c4a93105c
JiXiangLongBoy <1020165019@qq.com>
2019-08-05 19:44:18 +0800

    第二版项目

---
+++ src/main/java/com/shopplus/ShopplusApplication.java
@@ -0,0 +1,13 @@
+package com.shopplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopplus/ShopplusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shopplus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopplusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
